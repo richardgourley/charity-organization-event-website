8ec4f2d136ded020efc3822553f933d047cca5e5
Rich <codeleap@outlook.com>
2021-03-01 17:50:44 +0100

    Main site settings, static, templates and url file added

---
+++ charityorganizationsass/settings/dev.py
@@ -0,0 +1,24 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', cast=bool)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+# CONSOLE EMAIL BACKEND - TESTING
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# FILE BASED EMAIL BACKEND - TESTING
+# We are using filebased emails in a folder called /sentemails for testing purposes
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR + '/sent_emails')
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
