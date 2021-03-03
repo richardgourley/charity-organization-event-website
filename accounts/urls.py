35b5624db35a07670d97eacf2bc1ff702c582fdf
Rich <codeleap@outlook.com>
2021-03-03 20:50:15 +0100

    editprofile/ and profile/ urls added to accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, edit_custom_user_profile, account_profile_page
 
+app_name = 'accounts'
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('editprofile/', edit_custom_user_profile, name='editprofile'),
+    path('profile/', account_profile_page, name='profile'),
 ]
