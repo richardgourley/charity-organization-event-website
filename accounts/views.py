320302802aafcceb0fd80828d85717ecbb2f597d
Rich <codeleap@outlook.com>
2021-03-03 20:45:30 +0100

    SignUpView template location changed

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,6 @@ from .forms import CustomUserCreationForm
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    template_name = 'accounts/signup.html'
 
 # Create your views here.
