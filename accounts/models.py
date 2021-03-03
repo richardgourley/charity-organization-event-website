c5492c4bc0b496a46a8a690614d9c312bc6daa18
Rich <codeleap@outlook.com>
2021-03-03 20:49:47 +0100

    Charity details, country and continent select fields added

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,33 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    charity_name = models.CharField(max_length=255)
+    charity_address_line_1 = models.CharField(max_length=500)
+    charity_address_line_2 = models.CharField(max_length=500)
+    charity_postcode = models.CharField(max_length=50)
+    charity_website_url = models.URLField(help_text="Please enter a full URL ensurint it starts with 'http://www...")
+    charity_bio = models.TextField(max_length=2000)
+
+    charity_country = CountryField()
+
+    CONTINENT = (
+        ('oc', 'We operate mainly in the country the charity is based.'),
+        ('af', 'Africa'),
+        ('as', 'Asia'),
+        ('au', 'Australia/ Oceania'),
+        ('eu', 'Europe'),
+        ('na', 'North America'),
+        ('sa', 'South America'),
+    )
+
+    charity_operating_continent = models.CharField(
+        max_length=2,
+        choices=CONTINENT,
+        default='oc',
+        help_text='Which continent of the world do you mainly operate in? (Alternatively, select "We operate mainly in the country the charity is based.")'
+    )
 
     def __str__(self):
         return self.username
