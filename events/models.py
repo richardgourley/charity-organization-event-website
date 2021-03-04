566a114ade9d5ff6507f970b045985634d474b07
Rich <codeleap@outlook.com>
2021-03-04 20:05:34 +0100

    Events model added

--- events/models.py
+++ events/models.py
@@ -1,2 +1,6 @@
 from django.db import models
 from django.conf import settings
+
+class Event(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=255)
