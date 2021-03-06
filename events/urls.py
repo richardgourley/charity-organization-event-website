f3bbac0dce549442fe9563b3751b6e15b9e3aaf5
Rich <codeleap@outlook.com>
2021-03-06 15:30:59 +0100

    urlpatterns added to events.urls

--- events/urls.py
+++ events/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
 from .views import EventListView, EventDetailView, update_event_view, create_event_view
+
+app_name = 'events'
+urlpatterns = [
+    path('', EventListView.as_view(), name='all_events'),
+    path('update/<slug:slug>', update_event_view, name='update_event'),
+    path('detail/<slug:slug>', EventDetailView.as_view(), name='event_detail'),
+    path('create/', create_event_view, name='create_event'),
+]
