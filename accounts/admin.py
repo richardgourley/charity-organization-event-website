93a0224dd853294240cab080356f8df0e187081a
Rich <codeleap@outlook.com>
2021-03-08 11:59:16 +0100

    approved and charity_name fields added to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
+
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
-# Register your models here.
 class CustomUserAdmin(UserAdmin):
 	add_form = CustomUserCreationForm
 	form = CustomUserChangeForm
 	model = CustomUser
-	list_display = ['id', 'email', 'username']
+	list_display = ['charity_name', 'email', 'username', 'approved', 'is_staff']
+	
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
