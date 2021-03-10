11c62972441e4dfb6f99fedd73b847d64d62570c
Rich <codeleap@outlook.com>
2021-03-10 14:32:31 +0100

    Widgets for styling added to events form

--- events/forms.py
+++ events/forms.py
@@ -6,4 +6,9 @@ from .models import Event
 class EventForm(ModelForm):
 	class Meta:
 		model = Event
-		exclude = ["user", "slug"]
+		exclude = ["user", "slug", "approved"]
+
+		widgets = {
+			'event_name':forms.TextInput(attrs={'class':'form-control mb-4 border'}),
+			'event_description':forms.Textarea(attrs={'class':'form-control mb-4 border'}),
+		}
