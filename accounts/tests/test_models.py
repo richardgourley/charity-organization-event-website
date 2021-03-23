1d9ba1f2ecaa0fe205a08f13fb19a887ead40a61
Rich <codeleap@outlook.com>
2021-03-23 15:35:50 +0100

    Test accounts dir setup

---
+++ accounts/tests/test_models.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from datetime import datetime
+#from .models import Event
+from accounts.models import CustomUser
+
+class CustomUserModelTests(TestCase):
+	@classmethod
+	def setUpTestData(cls):
+		#Create Custom User
+		CustomUser.objects.create(
+            username="test_user_1",
+            email="email@testcharity.com",
+            password='test_user_1',
+            charity_name="Test Charity",
+            charity_address_line_1="Main Road",
+            charity_address_line_2="London",
+            charity_postcode="LON234",
+            charity_website_url="http://www.testcharity.com",
+            charity_bio="A test charity number 1",
+            charity_country='AU',
+            charity_operating_continent="oc",
+            charity_image="test_user_image.jpg"
+        )
+
+    def test_custom_user_details_saved_correctly(self):
+    	custom_user = CustomUser.objects.get(username="test_user_1")
+    	self.assertEqual(custom_user.username, "test_user_1")
+    	self.assertEqual(custom_user.email, "email@test_charity.com")
+    	self.assertEqual(custom_user.charity_name, "Test Charity")
+    	self.assertEqual(custom_user.charity_bio, "A test charity number 1")
