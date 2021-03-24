aaabb128a96f27792a8de6a98b50c06fe6b35439
Rich <codeleap@outlook.com>
2021-03-24 19:11:35 +0100

    Address fields tests added

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -29,7 +29,17 @@ class CustomUserModelTests(TestCase):
     	self.assertEqual(custom_user.charity_name, "Test Charity")
     	self.assertEqual(custom_user.charity_bio, "A test charity number 1")
 
-    def test_custom_user_charity_name_max_length(self):
+    def test_charity_name_max_length(self):
         custom_user = CustomUser.objects.get(username="test_user_1")
         max_length = custom_user._meta.get_field('charity_name').max_length
         self.assertEqual(max_length, 255)
+
+    def test_charity_address_line_1_max_length(self):
+        custom_user = CustomUser.objects.get(username="test_user_1")
+        max_length = custom_user._meta.get_field('charity_address_line_1').max_length
+        self.assertEqual(max_length, 500)
+
+    def test_charity_address_line_2_max_length(self):
+        custom_user = CustomUser.objects.get(username="test_user_1")
+        max_length = custom_user._meta.get_field('charity_address_line_2').max_length
+        self.assertEqual(max_length, 500)
