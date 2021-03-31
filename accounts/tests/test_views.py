309923e610fef92000b74be6e23e4767daae2d48
Rich <codeleap@outlook.com>
2021-03-31 11:02:31 +0200

    Edit custom user profile view test class added

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -35,3 +35,23 @@ class SignUpViewTests(TestCase):
         response = self.client.get(reverse('accounts:signup'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'accounts/signup.html')
+
+class EditCustomUserProfileTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        #Create Approved Custom User
+        CustomUser.objects.create(
+            username="test_user_1",
+            email="email@testcharity.com",
+            password='test_user_1',
+            charity_name="Test Charity",
+            charity_address_line_1="Main Road",
+            charity_address_line_2="London",
+            charity_postcode="LON234",
+            charity_website_url="http://www.testcharity.com",
+            charity_bio="A test charity number 1",
+            charity_country='AU',
+            charity_operating_continent="oc",
+            charity_image="test_user_image.jpg",
+            approved=True
+        )
