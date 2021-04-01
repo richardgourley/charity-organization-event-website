2d3f6f992bea6464239ba6e1ac0d664a3e7102e5
Rich <codeleap@outlook.com>
2021-04-01 19:43:48 +0200

    3 test events added to class setup

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -9,7 +9,58 @@ VIEW TESTS
 class EventViewTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        pass
+        # Create CustomUser
+        CustomUser.objects.create(
+                username="test_user_1",
+                email="email@testcharity.com",
+                password='test_user_1',
+                charity_name="Test Charity",
+                charity_address_line_1="Main Road",
+                charity_address_line_2="London",
+                charity_postcode="LON234",
+                charity_website_url="http://www.testcharity.com",
+                charity_bio="A test charity number 1",
+                charity_country='AU',
+                charity_operating_continent="oc",
+                charity_image="test_user_image.jpg"
+            )
+
+        # Get test_user_1
+        test_user_1 = CustomUser.objects.get(username="test_user_1")
+
+        # Create 3 Events
+        Event.objects.create(
+                user=test_user_1,
+                event_name='Approved event',
+                event_description='A lovely event.',
+                event_date=timezone.now() + datetime.timedelta(days=10),
+                event_url='http://www.testevent.com/approved_event',
+                approved=True,
+                image='image1.jpg',
+                slug='88_test_charity_approved_event',
+            )
+
+        Event.objects.create(
+                user=test_user_1,
+                event_name='Unapproved event',
+                event_description='Another lovely event.',
+                event_date=timezone.now() + datetime.timedelta(days=10),
+                event_url='http://www.testevent.com/unapproved_event',
+                approved=False,
+                image='image2.jpg',
+                slug='88_test_charity_unapproved_event',
+            )
+
+        Event.objects.create(
+                user=test_user_1,
+                event_name='Date in the past event',
+                event_description='An event with a date in the past.',
+                event_date=timezone.now() - datetime.timedelta(weeks=6),
+                event_url='http://www.testevent.com/date_in_the_past_event',
+                approved=True,
+                image='image3.jpg',
+                slug='75_test_charity_date_in_the_past_event',
+            )
 
     def test_home_page_status_code(self):
         response = self.client.get('/')
