c34a7c47362964335c25e89828adb5d5a8a39121
Rich <codeleap@outlook.com>
2021-04-02 17:22:46 +0200

    Event by unapproved user added

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -1,11 +1,9 @@
 from django.test import TestCase
 from datetime import datetime
+from django.utils import timezone
 from .models import Event
 from accounts.models import CustomUser
 
-'''
-VIEW TESTS
-'''
 class EventViewTests(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -25,8 +23,44 @@ class EventViewTests(TestCase):
                 charity_image="test_user_image.jpg"
             )
 
-        # Get test_user_1
+        # Create Unapproved Custom User
+        # Approved = FALSE by default
+        CustomUser.objects.create(
+            username="test_user_2",
+            email="email@testcharity.com",
+            password='test_user_2',
+            charity_name="Test Charity 2",
+            charity_address_line_1="Main Road",
+            charity_address_line_2="Edinburgh",
+            charity_postcode="LON234",
+            charity_website_url="http://www.testcharity2.com",
+            charity_bio="A test charity number 2",
+            charity_country='AU',
+            charity_operating_continent="oc",
+            charity_image="test_user_image.jpg"
+        )
+
+        #Create Staff Member - is_staff = True, approved = False (default)
+        CustomUser.objects.create(
+            username="staff_member",
+            email="staff_member@testcharity.com",
+            password='staff_member',
+            charity_name="None",
+            charity_address_line_1="None",
+            charity_address_line_2="None",
+            charity_postcode="None",
+            charity_website_url="http://www.charityorganizationsass.com",
+            charity_bio="None",
+            charity_country='AU',
+            charity_operating_continent="oc",
+            charity_image="test_user_image.jpg",
+            is_staff=True
+        )
+
+        # Get test_user_1 - APPROVED USER
         test_user_1 = CustomUser.objects.get(username="test_user_1")
+        # Get test_user_2 - UNAPPROVED USER
+        test_user_2 = CustomUser.objects.get(username="test_user_2")
 
         # Create 3 Events
         Event.objects.create(
@@ -62,6 +96,14 @@ class EventViewTests(TestCase):
                 slug='75_test_charity_date_in_the_past_event',
             )
 
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEquals(response.status_code, 200)
+        # Event somehow approved BUT created by unapproved user
+        Event.objects.create(
+                user=test_user_2,
+                event_name="Unapproved user event",
+                event_description="An event by unapproved user.",
+                event_date=timezone.now() + datetime.timedelta(days=10),
+                event_url='http://www.testevent.com/unapproved_user_event',
+                approved=True,
+                image='image4.jpg',
+                slug='unapproved_user_event',
+            )
