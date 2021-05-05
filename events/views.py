0fc67845703de6077b1aac7ac9443620f6294d95
Rich <codeleap@outlook.com>
2021-05-05 17:46:14 +0200

    Event list - context object name and pagination added

--- events/views.py
+++ events/views.py
@@ -18,6 +18,8 @@ import random
 class EventListView(generic.ListView):
     model = Event
     template_name = 'events/all_events.html'
+    context_object_name = 'events'
+    paginate_by = 2
 
     def get_queryset(self):
         return Event.objects.all().filter(approved=True).filter(event_date__gte=timezone.now()).order_by('event_date')
